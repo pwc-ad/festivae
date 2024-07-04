b02ffa18919582fb8e34799bc90f4eb029473001
RoyEden <roy.ede@gmail.com>
2024-07-03 23:50:12 -0300

    Update: Further image optimizations.

--- src/lib/utils/astro.ts
+++ src/lib/utils/astro.ts
@@ -1,6 +1,9 @@
 export function astroImageWidthsToMatchingSizes(widths: Array<number>) {
   return `${widths
     .slice(0, -1)
-    .map((width) => `(max-width: ${width}px) ${width}px`)
+    .map(
+      (width, index) =>
+        `(max-width: ${index ? widths[index + 1] : Math.round(width * (6 / 5))}px) ${width}px`,
+    )
     .join(",")}, ${widths.slice(-1)[0]}px`;
 }
