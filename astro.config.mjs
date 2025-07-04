1fa199fed3fe15b197cf384d29f29f972bff68f0
Ignacio Guerra <iguerra.dev@gmail.com>
2025-07-04 13:30:23 -0300

    Theme configration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
@@ -10,6 +8,19 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Roboto Condensed",
+        cssVariable: "--font-roboto-condensed"
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Turret Road",
+        cssVariable: "--font-turret-road"
+      }
+    ],
+  },
   integrations: [icon()]
 });
