4f96b1506adbcbf6faed63237cc2f485d75af7a6
RoyEden <roy.ede@gmail.com>
2025-07-11 13:18:21 -0300

    Add icons and social links

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,7 @@
-export const DATE_UTC = new Date(Date.UTC(2025, 6, 19, 18))
-export const DATE_DISPLAY = DATE_UTC.toLocaleDateString('es-AR', { day: 'numeric', month: 'long', year: 'numeric', timeZone: 'utc' })
+export const DATE_UTC = new Date(Date.UTC(2025, 6, 19, 18));
+export const DATE_DISPLAY = DATE_UTC.toLocaleDateString("es-AR", {
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+  timeZone: "utc",
+});
--- src/utils/formattedDate.ts
+++ src/utils/formattedDate.ts
@@ -1,17 +1,15 @@
-import { DATE_UTC } from "../lib/constants"
- 
-export const formattedDate = ({ withTime }: { withTime?:boolean } = {}) => {
-  const date = DATE_UTC.toLocaleDateString('es-AR', 
-    {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric',
-      timeZone: 'utc',
-      hour: withTime ? 'numeric' : undefined,
-      minute: withTime ? 'numeric' : undefined,
-      hour12: false,
-    }
-  )
+import { DATE_UTC } from "../lib/constants";
+
+export const formattedDate = ({ withTime }: { withTime?: boolean } = {}) => {
+  const date = DATE_UTC.toLocaleDateString("es-AR", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    timeZone: "utc",
+    hour: withTime ? "numeric" : undefined,
+    minute: withTime ? "numeric" : undefined,
+    hour12: false,
+  });
 
   return withTime ? `${date} hs.` : date;
-}
+};
