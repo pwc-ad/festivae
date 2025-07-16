ab951e2a831c4a95186a59c61a5804fc521707f1
RoyEden <roy.ede@gmail.com>
2025-07-16 19:17:21 -0300

    Update: add background and glowing text/icons

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const DATE_UTC = new Date(Date.UTC(2025, 6, 19, 18));
+export const DATE_UTC = new Date(Date.UTC(2025, 6, 19, 17));
 export const DATE_DISPLAY = DATE_UTC.toLocaleDateString("es-AR", {
   day: "numeric",
   month: "long",
