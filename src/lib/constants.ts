5acc171d2151c4b1f9b8c4db0a508a796006011d
RoyEden <roy.ede@gmail.com>
2025-11-30 01:46:15 -0300

    Edición de diciembre

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const DATE_UTC = new Date(Date.UTC(2025, 6, 19, 17));
+export const DATE_UTC = new Date(Date.UTC(2025, 11, 6, 17));
 export const DATE_DISPLAY = DATE_UTC.toLocaleDateString("es-AR", {
   day: "numeric",
   month: "long",
