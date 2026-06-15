cfc3acf6253aca56f18c9f068ec5ca6840542eb8
RoyEden <roy.ede@gmail.com>
2026-06-15 14:25:38 -0300

    chore: Updated to astro@latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,26 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import icon from 'astro-icon';
+import { defineConfig, fontProviders } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.google(),
-        name: "Roboto Condensed",
-        cssVariable: "--font-roboto-condensed"
-      },
-      {
-        provider: fontProviders.google(),
-        name: "Turret Road",
-        cssVariable: "--font-turret-road"
-      }
-    ],
-  },
-  integrations: [icon(), svelte()]
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Roboto Condensed",
+      cssVariable: "--font-roboto-condensed",
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Turret Road",
+      cssVariable: "--font-turret-road",
+    },
+  ],
+  integrations: [icon(), svelte()],
 });
